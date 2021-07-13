a244e9aba73ce975d24beebf6e1da0e5d7b679ab
FriendGlak <camilojheans@gmail.com>
2021-07-12 22:17:05 -0500

    Correción #7

--- server.js
+++ server.js
@@ -38,4 +38,4 @@ app.post('/create-checkout-session', async (req, res) => {
   res.redirect(303, session.url)
 });
 
-app.listen(4242, () => console.log('Running on port 4242'));
+app.listen((config.PORT || 5000), () => console.log('Running on port 4242'));
